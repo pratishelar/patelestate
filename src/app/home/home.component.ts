024b1a994c0dcaf85e8ffbe7c8fa6552f0c22d17
admin <admin@MacBook-Pro.local>
2021-01-14 16:22:33 -0500

    UI Changes home property page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MouseEvent } from '@agm/core';
+
 
 @Component({
   selector: 'app-home',
@@ -11,48 +11,5 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {}
 
-  // google maps zoom level
-  zoom: number = 8;
-  icon = { url: '../assets/images/pin.png', scaledSize: {height: 40, width: 30}}
-
-  // initial center position for the map
-  lat: number = 51.673858;
-  lng: number = 7.815982;
-
-  clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}`);
-  }
-
-  mouseOver(m: any, i: number) {
-    // m.icon = '../assets/images/pin3.png';
-  }
-
-  markers: any[] = [
-    {
-      lat: 51.673858,
-      lng: 7.815982,
-      label: 'A',
-      draggable: false,
-    },
-    {
-      lat: 51.373858,
-      lng: 7.215982,
-      label: 'B',
-      draggable: false,
-    },
-    {
-      lat: 51.723858,
-      lng: 7.895982,
-      label: 'C',
-      draggable: false,
-    },
-  ];
 }
 
-// just an interface for type safety.
-interface marker {
-  lat: number;
-  lng: number;
-  label?: string;
-  draggable: boolean;
-}
--- src/app/propertiesFullGrid/propertiesFullGrid.component.ts
+++ src/app/propertiesFullGrid/propertiesFullGrid.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PropertiesService } from '../_services/properties.service';
 import { ActivatedRoute } from '@angular/router';
 import { TimeagoIntl } from 'ngx-timeago';
+import { MouseEvent } from '@agm/core';
 
 @Component({
   selector: 'app-propertiesFullGrid',
@@ -36,4 +37,51 @@ export class PropertiesFullGridComponent implements OnInit {
     //   }
     // );
   }
+
+
+  // google maps zoom level
+  zoom: number = 8;
+  icon = { url: '../assets/images/pin.png', scaledSize: {height: 40, width: 30}}
+
+  // initial center position for the map
+  lat: number = 51.673858;
+  lng: number = 7.815982;
+
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`);
+  }
+
+  mouseOver(m: any, i: number) {
+    // m.icon = '../assets/images/pin3.png';
+  }
+
+  markers: any[] = [
+    {
+      lat: 51.673858,
+      lng: 7.815982,
+      label: 'A',
+      draggable: false,
+    },
+    {
+      lat: 51.373858,
+      lng: 7.215982,
+      label: 'B',
+      draggable: false,
+    },
+    {
+      lat: 51.723858,
+      lng: 7.895982,
+      label: 'C',
+      draggable: false,
+    },
+  ];
+}
+
+
+// just an interface for type safety.
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
 }
